e6f601c60972dc02da45101b39e9434588c9fac8
Lincy R <lincy.r.s74@kalvium.community>
2025-01-31 10:15:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/ping', (req, res) => {
-    res.send('pong');
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
 
+const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(Server running on http://localhost:${PORT});
 });
