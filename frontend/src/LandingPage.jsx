81ecd1b69ec53a99648fd3d4c4dc4a4b29807390
lincyrajasekaran3 <hp@DESKTOP-6433J6U>
2025-03-11 11:09:45 +0530

    mongo connection successful

---
+++ frontend/src/LandingPage.jsx
@@ -0,0 +1,12 @@
+
+
+function LandingPage() {
+    return (
+        <div style={{ textAlign: "center", padding: "50px" }}>
+            <h1>Welcome to the ASAP Project</h1>
+            <p>This is the landing page for our ASAP idea.</p>
+        </div>
+    );
+}
+
+export default LandingPage;
